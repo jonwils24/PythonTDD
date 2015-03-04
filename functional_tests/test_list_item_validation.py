224a61fa41e1b7132879d2c3894a3743c343ead1
Jonathan Wilson <jonathan.wilson@alfresco.com>
2015-03-04 11:22:38 -0800

    Fixed DB issue

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -6,10 +6,12 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empyt_list_items(self):
         # Edith goes to the home page and accidently tries to submit
         # an empty list item. She hits Enter on the empty input box
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
-
+        error = self.browser.find_element_by_css_selector('.has-error')
         # She tries again with some text for the item, which now works
 
         # Perversely, she now decides to submit a second blank list item
